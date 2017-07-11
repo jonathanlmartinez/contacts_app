8fcfb41b5b62cc847525e5859b9f3b1db4da0d22
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-07-11 18:40:51 -0500

    added index and show

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  
+  def index
+    @contacts = Contact.all
+    @first = Contact.find_by(id:1)
+    @second = Contact.find_by(id:2)
+    @third = Contact.find_by(id:3)
+    render "index.html.erb"
+  end 
+
+  def show
+    id = params[:id]
+    contact = Contact.find_by(id: id)
+    @first_name = contact.first_name
+    @last_name = contact.last_name
+    @email = contact.email
+    @phone_number = contact.phone_number
+    render "show.html.erb"
+  end 
+
+end
