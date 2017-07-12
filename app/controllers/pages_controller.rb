dbc3e362a03c2955df86ff227d275e125dc981a1
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-07-11 19:49:50 -0500

    edit and patch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,11 +10,8 @@ class PagesController < ApplicationController
 
   def show
     id = params[:id]
-    contact = Contact.find_by(id: id)
-    @first_name = contact.first_name
-    @last_name = contact.last_name
-    @email = contact.email
-    @phone_number = contact.phone_number
+    @contact = Contact.find_by(id: id)
+   
     render "show.html.erb"
   end 
 
@@ -23,8 +20,37 @@ class PagesController < ApplicationController
   end
 
   def create
-    
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number:[:phone_number]
+    )
+    contact.save
+
     render "create.html.erb"
   end 
 
+  def edit 
+    @id = params[:id]
+    contact = Contact.find_by(id: @id)
+    @first_name = contact.first_name
+    @last_name = contact.last_name
+    @email = contact.email
+    @phone_number = contact.phone_number
+    render "edit.html.erb"
+  end
+
+  def update
+    id = params[:id]
+    contact = Contact.find_by(id: id)
+    contact.first_name = params[:first_name]
+    contact.last_name = params[:last_name]
+    contact.email = params[:email]
+    contact.phone_number = params[:phone_number]
+    
+    contact.save
+
+    render "update.html.erb"
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/index" => "pages#index"
-  get "/index/new" => "pages#new"
-  post "/index" => "pages#create"
-  get "/index/:id" => "pages#show"
+  get "/contacts" => "pages#index"
+  get "/contacts/new" => "pages#new"
+  post "/contacts" => "pages#create"
+  get "/contacts/:id" => "pages#show"
+  get "contacts/:id/edit" => "pages#edit"
+  patch "/contacts/:id" => "pages#update"
 end
